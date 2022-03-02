661ee0310191d3ba5ba5267f605085c8bbf580a1
ST00810790 <100113179+ST00810790@users.noreply.github.com>
2022-03-02 12:07:51 +0000

    Add files via upload

---
+++ demo/src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Course.java
@@ -0,0 +1,57 @@
+package com.in28minutes.jpa.hibernate.demo.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+
+@Entity
+
+
+public class Course {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	@Column(nullable=false)
+	private String name;
+	
+	@UpdateTimestamp
+	private LocalDateTime lastUpdatedDate;
+	
+	@CreationTimestamp
+	private LocalDateTime createdDate;
+
+	protected Course() {
+	}
+
+	public Course(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Course [%s]",name);
+	}
+	
+}
